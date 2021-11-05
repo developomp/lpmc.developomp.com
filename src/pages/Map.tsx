8badee5781aea10a858aae26b2ea9e9e8860b661
developomp <developomp@gmail.com>
2021-11-05 18:56:31 +0900

    fixed scroll bar showing on map page

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -2,8 +2,14 @@ import { Helmet } from "react-helmet-async"
 import styled from "styled-components"
 
 const StyledMap = styled.object`
+	position: absolute;
 	width: 100%;
-	height: calc(100vh - 5rem); /* find a less hacky solution */
+	height: calc(100vh - 5rem);
+
+	top: 5rem;
+	right: 0px;
+	bottom: 0px;
+	left: 0px;
 `
 
 function Map() {
